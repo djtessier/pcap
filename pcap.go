a6f0a86bf62ecbcf53ee7399c5150d50ea022239
Tony Huinker <ahuinker@gmail.com>
2016-02-07 12:48:37 -0500

    Made changes from the development team feedback

--- pcap.go
+++ pcap.go
@@ -19,7 +19,6 @@ import (
 )
 
 const (
-	//e
 	APIKey string = "ExtraHop apikey=d886220a756f4f22847d58b17e269dea"
 	Path   string = "https://192.168.1.120/api/v1/"
 
@@ -27,36 +26,28 @@ const (
 //	Path   string = "https://10.6.105.231/api/v1/"
 )
 
-// PrettyPrint is an example of what can be the done with the results
-var clear map[string]func() //create a map for storing clear funcs
+var (
+	count     = 0
+	set       = make(map[string]string)
+	triggerID = -1
+)
 
-func init() {
-	clear = make(map[string]func()) //Initialize it
-	clear["linux"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-	clear["darwin"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-	clear["windows"] = func() {
-		cmd := exec.Command("cls") //Windows example it is untested, but I think its working
-		cmd.Stdout = os.Stdout
-		cmd.Run()
+// cleanup attempts to delete any created trigger
+func cleanup() {
+	if triggerID != -1 {
+		CreateEhopRequest("DELETE", "triggers/"+fmt.Sprint(float64(triggerID)), "none")
 	}
 }
 
-func CallClear() {
-	fmt.Println(runtime.GOOS)
-	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-	if ok {                          //if we defined a clear func for that platform:
-		value() //we execute it
-	} else { //unsupported platform
-		panic("Your platform is unsupported! I can't clear terminal screen :(")
-	}
+// terminate prints a fatal log message after attempting to delete any created
+// trigger.
+func terminate(message string) {
+	cleanup()
+	log.Fatal(message)
+}
+func terminatef(message string, v ...interface{}) {
+	cleanup()
+	log.Fatalf(message, v...)
 }
 func PrettyPrint(data interface{}) {
 	b, err := json.MarshalIndent(data, "", "  ")
@@ -74,12 +65,11 @@ func CreateEhopRequest(method string, call string, payload string) *http.Respons
 	}
 	//Crate a new client object... and pass him the parameters of the transport object
 	//we created above
-
 	client := http.Client{Transport: tr}
 	postBody := []byte(payload)
 	req, err := http.NewRequest(method, Path+call, bytes.NewBuffer(postBody))
 	if err != nil {
-		log.Fatalf("Failed to create HTTP request: %q", err.Error())
+		terminatef("Failed to create HTTP request: %q", err.Error())
 	}
 
 	//Add some header stuff to make it EH friendly
@@ -87,33 +77,34 @@ func CreateEhopRequest(method string, call string, payload string) *http.Respons
 	req.Header.Add("Content-Type", " application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Failed to perform HTTP request: %q", err.Error())
+		terminatef("Failed to perform HTTP request: %q", err.Error())
 	}
-	//	defer resp.Body.Close()
 	return resp
 }
-func ConvertResponseToJsonArray(resp *http.Response) []map[string]interface{} {
+func ConvertResponseToJSONArray(resp *http.Response) []map[string]interface{} {
 	// Depending on the request, you may not need an array
 	//var results = make(map[string]interface{})
 	var mapp = make([]map[string]interface{}, 0)
 	if err := json.NewDecoder(resp.Body).Decode(&mapp); err != nil {
-		log.Fatalf("Could not parse results: %q", err.Error())
+		terminatef("Could not parse results: %q", err.Error())
 	}
 	defer resp.Body.Close()
 	return mapp
 }
 
-func GetPcaps(answer2 string) {
-	for cont == true {
-		time.Sleep(2 * time.Second)
-		response := CreateEhopRequest("GET", "packetcaptures", "none")
-		results := ConvertResponseToJsonArray(response)
-		for _, value := range results {
-			if value["name"] == answer2 {
-				if set[value["id"].(string)] == "" {
+func GetPcaps(sessionName string, finish <-chan bool) {
+	ticker := time.NewTicker(time.Second * 2).C
+	for {
+		select {
+		case <-finish:
+			return
+		case <-ticker:
+			response := CreateEhopRequest("GET", "packetcaptures", "none")
+			results := ConvertResponseToJSONArray(response)
+			for _, value := range results {
+				if value["name"] == sessionName && set[value["id"].(string)] == "" {
 					set[value["id"].(string)] = value["ipaddr1"].(string) + " " + fmt.Sprint(float64(value["port1"].(float64))) + " --- " + value["ipaddr2"].(string) + " " + fmt.Sprint(float64(value["port2"].(float64))) + " " + value["l7proto"].(string)
-					//fmt.Sprint(float64(triggerID))
-					count = count + 1
+					count++
 					fmt.Printf("%d %s \n", count, set[value["id"].(string)])
 				}
 			}
@@ -121,93 +112,80 @@ func GetPcaps(answer2 string) {
 	}
 }
 
-var count = 0
-var set = make(map[string]string)
-var cont = true
+// askForInput prompts the reader for a string input then thanks them for their
+// cooperation. The result is stripped of leading or trailing whitespace.
+func askForInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(prompt)
+	response, _ := reader.ReadString('\n')
+	fmt.Println("\nThank You")
+	return strings.TrimSpace(response)
+}
+
+// createTrigger executes a POST request to create a trigger and returns the
+// resulting triggerID. If there was an error the triggerID is set to -1
+func createTrigger(script, sessionName string) int {
+	CreateEhopRequest("POST", "triggers", script)
+	response := CreateEhopRequest("GET", "triggers", "none")
+	results := ConvertResponseToJSONArray(response)
+	for _, value := range results {
+		if value["name"] == sessionName {
+			return int(value["id"].(float64))
+		}
+	}
+	return -1
+}
 
 func main() {
-	triggerID := 0
-	response := CreateEhopRequest("GET", "devices?search_type=any", "none")
-	results := ConvertResponseToJsonArray(response)
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Please enter a name to be used for this session. (Single Word Only Please)")
-	answer2, _ := reader.ReadString('\n')
-	answer2 = strings.TrimSpace(answer2)
-	fmt.Println("\nThank You")
-	time.Sleep(2 * time.Second)
+	sessionName := askForInput("Please enter a name to be used for this session. (Single Word Only Please)")
 	fmt.Println("A -- > To capture all packets to a single IP address")
 	fmt.Println("B -- > To capture all packets sent between 2 IP addresses")
 	answer5, _ := reader.ReadString('\n')
-	//script := ""
 
 	if strings.TrimSpace(answer5) == "A" {
-		fmt.Println("Please enter the IP address of the server you would like to do a packet capture on")
-		answer, _ := reader.ReadString('\n')
-		answer = strings.TrimSpace(answer)
-		fmt.Println("\nThank You")
-		time.Sleep(2 * time.Second)
-		code := `if(Flow.client.ipaddr.toString() == '` + answer + `' || Flow.server.ipaddr.toString() == '` + answer + `'){\nFlow.captureStart('` + answer2 + `');\n}`
-		var script string = `{ "apply_all": true, "author": "GO", "debug": false, "description": "Scripted PCAP", "disabled": false, "event": "FLOW_CLASSIFY", "hints": {"packetCapture": true}, "name": "` + answer2 + `", "priority": 0, "script": "` + code + `" }`
-		response = CreateEhopRequest("POST", "triggers", script)
-		response = CreateEhopRequest("GET", "triggers", "none")
-		results = ConvertResponseToJsonArray(response)
-		for _, value := range results {
-			if value["name"] == answer2 {
-				triggerID = int(value["id"].(float64))
-			}
-		}
+		firstIP := askForInput("Please enter the IP address of the server you would like to do a packet capture on")
+		code := `if(Flow.client.ipaddr.toString() == '` + firstIP + `' || Flow.server.ipaddr.toString() == '` + firstIP + `'){\nFlow.captureStart('` + sessionName + `');\n}`
+		script := `{ "apply_all": true, "author": "GO", "debug": false, "description": "Scripted PCAP", "disabled": false, "event": "FLOW_CLASSIFY", "hints": {"packetCapture": true}, "name": "` + sessionName + `", "priority": 0, "script": "` + code + `" }`
+		triggerID = createTrigger(script, sessionName)
 	} else if strings.TrimSpace(answer5) == "B" {
-		fmt.Println("Please enter the first IP address of the server you would like to do a packet capture on")
-		answer, _ := reader.ReadString('\n')
-		answer = strings.TrimSpace(answer)
-		fmt.Println("\nThank You")
-		time.Sleep(2 * time.Second)
-		fmt.Println("Please enter the second IP address of the server you would like to do a packet capture on")
-		answer6, _ := reader.ReadString('\n')
-		answer6 = strings.TrimSpace(answer6)
-		fmt.Println("\nThank You")
-		time.Sleep(2 * time.Second)
-		code := `if(Flow.client.ipaddr.toString() == '` + answer + `' && Flow.server.ipaddr.toString() == '` + answer6 + `'){\nFlow.captureStart('` + answer2 + `');\n}\nif(Flow.client.ipaddr.toString() == '` + answer6 + `' && Flow.server.ipaddr.toString() == '` + answer + `'){\nFlow.captureStart('` + answer2 + `');\n }`
-		var script string = `{ "apply_all": true, "author": "GO", "debug": false, "description": "Scripted PCAP", "disabled": false, "event": "FLOW_CLASSIFY", "hints": {"packetCapture": true}, "name": "` + answer2 + `", "priority": 0, "script": "` + code + `" }`
-		response = CreateEhopRequest("POST", "triggers", script)
-		response = CreateEhopRequest("GET", "triggers", "none")
-		results = ConvertResponseToJsonArray(response)
-		for _, value := range results {
-			if value["name"] == answer2 {
-				triggerID = int(value["id"].(float64))
-			}
-		}
-
+		firstIP := askForInput("Please enter the first IP address of the server you would like to do a packet capture on")
+		secondIP := ("Please enter the second IP address of the server you would like to do a packet capture on")
+		code := `if(Flow.client.ipaddr.toString() == '` + firstIP + `' && Flow.server.ipaddr.toString() == '` + secondIP + `'){\nFlow.captureStart('` + sessionName + `');\n}\nif(Flow.client.ipaddr.toString() == '` + secondIP + `' && Flow.server.ipaddr.toString() == '` + firstIP + `'){\nFlow.captureStart('` + sessionName + `');\n }`
+		script := `{ "apply_all": true, "author": "GO", "debug": false, "description": "Scripted PCAP", "disabled": false, "event": "FLOW_CLASSIFY", "hints": {"packetCapture": true}, "name": "` + sessionName + `", "priority": 0, "script": "` + code + `" }`
+		triggerID = createTrigger(script, sessionName)
 	} else {
-
-		fmt.Println("Need to select either A or B")
+		terminate("Need to select either A or B")
 	}
+	if triggerID < 0 {
+		terminate("Could not create new trigger for packet captures")
+	}
+
 	fmt.Printf("Waiting a bit for captures to show up... Press 1 to Quit\n")
-	go GetPcaps(answer2)
-	answer3, _ := reader.ReadString('\n')
-	answer3 = strings.TrimSpace(answer3)
-	if answer3 == "1" {
-		cont = false
+	finish := make(chan bool)
+	go GetPcaps(sessionName, finish)
+	for {
+		killsignal, _ := reader.ReadString('\n')
+		if strings.TrimSpace(killsignal) == "1" {
+			finish <- true
+			break
+		}
 	}
 	filename := ""
 	counter := 1
 	os.Mkdir("."+string(filepath.Separator)+"pcap", 0777)
-	for value, _ := range set {
+	for value := range set {
 		filename = set[value]
-		//		fmt.Println("Downloading " + filename)
-		response = CreateEhopRequest("GET", "packetcaptures/"+value, "none")
-		filename = filename + strconv.Itoa(counter) + ".pcap"
-		counter = counter + 1
+		response := CreateEhopRequest("GET", "packetcaptures/"+value, "none")
+		fmt.Println("Downloading... " + filename)
+		filename += strconv.Itoa(counter) + ".pcap"
+		counter++
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(response.Body)
-		//file, _ := os.OpenFile("."+string(filepath.Separator)+"pcap"+string(filepath.Separator)+filename, os.O_CREATE, 0777)
-		//bufferedWriter := bufio.NewWriter(file)
-		//bufferedWriter.Write(buf.Bytes())
-		//bufferedWriter.Flush()
-		//file.Close()
 		ioutil.WriteFile("."+string(filepath.Separator)+"pcap"+string(filepath.Separator)+filename, buf.Bytes(), 0777)
-		defer response.Body.Close()
+		response.Body.Close()
 	}
+	fmt.Println("Download successful.... attempting to merge")
 	mergecap := ""
 	installed := false
 	if strings.Index(runtime.GOOS, "windows") > -1 {
@@ -241,7 +219,6 @@ func main() {
 		for y := range args {
 			if strings.Index(args[y], ",") > -1 {
 				args[y] = args[y][:len(args[y])-1]
-				fmt.Println(args[y])
 			}
 		}
 		//		cmd := exec.Command("mergecap", "-w", "combined.pcap", "pcap"+string(filepath.Separator)+"*")
@@ -251,10 +228,11 @@ func main() {
 		//test
 		err := cmd.Run()
 		if err != nil {
+			terminate("Failed during merge process...")
 			fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
 		} else {
 			fmt.Println("Successfully combined pcaps")
 		}
 	}
-	response = CreateEhopRequest("DELETE", "triggers/"+fmt.Sprint(float64(triggerID)), "none")
+	cleanup()
 }
